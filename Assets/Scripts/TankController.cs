52f637e0f17c5f2c20cfde6b75d3d60eb6bdb3a9
Syed zainul abedin <szsimplesyed93@gmail.com>
2024-07-20 13:21:13 +0530

    Added movement and rotation

--- Assets/Scripts/TankController.cs
+++ Assets/Scripts/TankController.cs
@@ -7,12 +7,25 @@ public class TankController
     private TankModel tankModel;
     private TankView tankView;
     
+    private Rigidbody rb;
+    
     public TankController(TankModel tankModel, TankView tankView)
     {
         this.tankModel = tankModel;
-        this.tankView = tankView;
-        this.tankModel.SetTankController(this);
+        this.tankView = GameObject.Instantiate<TankView>(tankView, Vector3.zero, Quaternion.identity);
         this.tankView.SetTankController(this);
-        GameObject.Instantiate(tankView, Vector3.zero, Quaternion.identity);
+        this.tankModel.SetTankController(this);
+        rb = this.tankView.GetRigidbody();
+    }
+
+    public void Move(float movement, float speed)
+    {
+        rb.velocity = tankView.transform.forward * movement * speed;
+    }
+    public void Rotate(float rotate, float speed)
+    {
+        Vector3 vector = new Vector3(0, rotate * speed, 0);
+        Quaternion deltaRotation = Quaternion.Euler(vector * Time.deltaTime);
+        rb.MoveRotation(rb.rotation * deltaRotation);
     }
 }
--- Assets/Scripts/TankView.cs
+++ Assets/Scripts/TankView.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,9 +6,33 @@ using UnityEngine;
 public class TankView : MonoBehaviour
 {
     private TankController tankController;
+
+    private float movement;
+    private float rotation;
+    
+    public Rigidbody rb;
+    
+    public Rigidbody GetRigidbody()
+    {
+        return rb;
+    }
     
     public void SetTankController(TankController tankController)
     {
         this.tankController = tankController;
+    }
+
+    private void Update()
+    {
+        movement = Input.GetAxis("Vertical");
+        rotation = Input.GetAxis("Horizontal");
+        if (movement != 0)
+        {
+            tankController.Move(movement, 30);
+        }
+        if (rotation != 0)
+        {
+            tankController.Rotate(rotation, 20);
+        }
     }
 }
